9a31316196b6cb37da711e11eaa604a4f45fb1b4
Sunny Gandhwani <sunnygandhwani027@gmail.com>
2021-08-24 19:14:48 +0530

    set format

--- backend/index.js
+++ backend/index.js
@@ -9,35 +9,35 @@ const bodyParser = require('body-parser');
 var app = express();
 app.use(cors());
 
-var client = new Mongoclient(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology:true});
+var client = new Mongoclient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 var connection;
-client.connect((err,db)=>{
-    if(!err){
+client.connect((err, db) => {
+    if (!err) {
         connection = db;
         console.log("Database Connected")
     }
-    else{
+    else {
         console.log("Error in Connecting Database")
     }
 })
 
-app.get('/list-user',(req,res)=>{ 
+app.get('/list-user', (req, res) => {
     var usercollection = connection.db('stackinflow').collection('user');
-    usercollection.find().toArray((err,docs)=>{
-        if(!err){
-            res.send({status:"ok",data:docs})
+    usercollection.find().toArray((err, docs) => {
+        if (!err) {
+            res.send({ status: "ok", data: docs })
         }
-        else{
-            res.send({status:"failed",data:err})
+        else {
+            res.send({ status: "failed", data: err })
         }
     })
 })
 
-app.post('/check-login', bodyparser.json() ,(req, res) => {
+app.post('/check-login', bodyparser.json(), (req, res) => {
     console.log(req.body);
     var usercollection = connection.db('stackinflow').collection('user');
-    usercollection.find({email:req.body.email, password:req.body.password}).toArray((err, result) => {
-        if (!err && result.length>0) {
+    usercollection.find({ email: req.body.email, password: req.body.password }).toArray((err, result) => {
+        if (!err && result.length > 0) {
             res.send({ status: 'ok', data: result[0] });
         } else {
             res.send({ status: 'error', data: err })
@@ -45,10 +45,10 @@ app.post('/check-login', bodyparser.json() ,(req, res) => {
     })
 })
 
-app.post('/valid-email', bodyparser.json() ,(req, res) => {
+app.post('/valid-email', bodyparser.json(), (req, res) => {
     var usercollection = connection.db('stackinflow').collection('user');
-    usercollection.find({email:req.body.email}).toArray((err, result) => {
-        if (!err && result.length>0) {
+    usercollection.find({ email: req.body.email }).toArray((err, result) => {
+        if (!err && result.length > 0) {
             res.send({ status: 'error', data: "this email is already registered" });
         } else {
             res.send({ status: 'ok' })
@@ -56,76 +56,72 @@ app.post('/valid-email', bodyparser.json() ,(req, res) => {
     })
 })
 
-app.get('/delete-user',(req,res)=>{ 
+app.get('/delete-user', (req, res) => {
     var usercollection = connection.db('stackinflow').collection('user')
-    usercollection.remove({_id:ObjectId(req.query.id)},(err,result)=>{
-        if(!err){
-            res.send({status:"ok",data:"user deleted successfully"})
+    usercollection.remove({ _id: ObjectId(req.query.id) }, (err, result) => {
+        if (!err) {
+            res.send({ status: "ok", data: "user deleted successfully" })
         }
-        else{
-            res.send({status:"failed",data:err})
+        else {
+            res.send({ status: "failed", data: err })
         }
     })
 })
 
-app.post('/create-user', bodyparser.json(),(req,res)=>{
+app.post('/create-user', bodyparser.json(), (req, res) => {
     var usercollection = connection.db('stackinflow').collection('user');
-        usercollection.insert(req.body,(err,result)=>{
-            if(!err){
-                // console.log(otp)
-                res.send({status:"ok",data:"user created successfully"})
-                sendMail("process.env.APP_ID", "process.env.APP_PASSWORD", req.body.email, "Welcome to stackinflow", `<h2>stackinflow</h2><br><h4> Registration SuccessFull </h4><br>We hope you find our service cool.`)
-            }
-            else{
-                res.send({status:"failed",data:err})
-            }
-        })
+    usercollection.insert(req.body, (err, result) => {
+        if (!err) {
+            // console.log(otp)
+            res.send({ status: "ok", data: "user created successfully" })
+            sendMail("process.env.APP_ID", "process.env.APP_PASSWORD", req.body.email, "Welcome to stackinflow", `<h2>stackinflow</h2><br><h4> Registration SuccessFull </h4><br>We hope you find our service cool.`)
+        }
+        else {
+            res.send({ status: "failed", data: err })
+        }
+    })
 })
 
-app.post("/send-otp-email",bodyParser.json(),(req,res)=>{
+app.post("/send-otp-email", bodyParser.json(), (req, res) => {
     console.log(req.body);
     sendMail("process.env.APP_ID", "process.env.APP_PASSWORD", req.body.email, "Welcome to stackinflow", `Your One Time Password is - <h3>${req.body.otp}</h3><br><h6>We hope you find our service cool.</h6>`)
-    res.send({status:"ok",data:"please verify your email"});
+    res.send({ status: "ok", data: "please verify your email" });
 })
 
 // app.post('/update-user',(req,res)=>{  })
 
-function sendMail(from, appPassword, to, subject,  htmlmsg)
-{
-    let transporter=nodemailer.createTransport(
+function sendMail(from, appPassword, to, subject, htmlmsg) {
+    let transporter = nodemailer.createTransport(
         {
-            host:"smtp.gmail.com",
-            port:587,
-            secure:false,
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
             auth:
             {
-             //  user:"weforwomen01@gmail.com",
-             //  pass:""
-            user:from,
-            pass:appPassword
+                //  user:"weforwomen01@gmail.com",
+                //  pass:""
+                user: from,
+                pass: appPassword
             }
         }
-      );
-    let mailOptions=
+    );
+    let mailOptions =
     {
-       from:from ,
-       to:to,
-       subject:subject,
-       html:htmlmsg
+        from: from,
+        to: to,
+        subject: subject,
+        html: htmlmsg
     };
-    transporter.sendMail(mailOptions ,function(error,info)
-    {
-      if(error)
-      {
-        console.log(error);
-      }
-      else
-      {
-        console.log('Email sent:'+info.response);
-      }
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log('Email sent:' + info.response);
+        }
     });
 }
 
-app.listen(3001,()=>{
+app.listen(3001, () => {
     console.log("Server is running on port 3001")
 })
