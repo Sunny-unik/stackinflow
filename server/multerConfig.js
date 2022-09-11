88063a267416877f85f08d25c2952cd338e29268
Sunny-unik <sunnygandhwani027@gmail.com>
2022-09-11 23:30:34 +0530

    update server format

--- server/multerConfig.js
+++ server/multerConfig.js
@@ -1,30 +1,35 @@
-var multer = require('multer');
-var path = require('path');
+const multer = require('multer');
+const path = require('path');
 
-var storage = multer.diskStorage(
-    {destination: function (req, file, cb) {
-        cb(null, 'uploads/userprofiles')
+const storage = multer.diskStorage(
+  {
+    destination: (req, file, callback) => {
+      callback(null, 'userProfiles');
     },
-        filename: function (req, file, cb) {
-            var ext = path.extname(file.originalname);
-            console.log("in config 12---trying to upload --"+file.fieldname.slice(0,3) + '-' + Date.now()+"."+ext)
-            cb(null, file.fieldname.slice(0,3) + '-' + Date.now()+"."+ext);
-        }
-    }
-    //   profile-234763458969876.jpg
-)
-var multerOptions = { storage: storage,
-    fileFilter: function(req,file,callback){
-        var ext = path.extname(file.originalname);
-        var fieldName = file.fieldname;
-        if(fieldName=="profile"){
-            if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
-                return callback(new Error(`Only png,jpg,jpeg extension's images are allowed for profile`));
-            }
-            callback(null, true);
-        }   
-    }
-}
-var upload = multer(multerOptions).fields([{name:"profile", maxCount:1}]);
+    filename: (req, file, cb) => {
+      const extension = path.extname(file.originalname);
+      console.log('Uploading ' + file.fieldname.slice(0, 3) + '-' + Date.now() + extension);
+      cb(null, file.fieldname.slice(0, 3) + '-' + Date.now() + extension);
+    },
+  }
+  //   profile-234763458969876.jpg
+);
 
-module.exports=upload;
+const multerOptions = {
+  storage: storage,
+  fileFilter: function (req, file, callback) {
+    const extension = path.extname(file.originalname);
+    const fieldName = file.fieldname;
+    if (fieldName == 'profile') {
+      if (extension !== '.png' && extension !== '.jpg' && extension !== '.jpeg') {
+        return callback(
+          new Error(`Only png, jpg, jpeg extension's images are allowed for profile`)
+        );
+      }
+      callback(null, true);
+    }
+  },
+};
+
+const upload = multer(multerOptions).fields([{ name: 'profile', maxCount: 1 }]);
+module.exports = upload;
