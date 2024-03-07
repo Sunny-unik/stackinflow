fab9125107e25e5028bca9fea9995ad6d0385591
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-03-07 12:00:22 +0530

    feat: add vscode/extensions.json & resolve mongoose warning

--- server/index.js
+++ server/index.js
@@ -21,6 +21,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "uploads/userProfiles")));
 
+mongoose.set("strictQuery", true);
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
