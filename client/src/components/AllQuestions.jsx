553341f0c3a4461d08ac44f2f925ef0d52c23e50
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-04-17 23:40:17 +0530

    feat: add seprate component for pagination with limit

--- client/src/components/AllQuestions.jsx
+++ client/src/components/AllQuestions.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Spinner from "./loadings/Spinner";
-import ReactPaginate from "react-paginate";
 import QuestionBox from "./QuestionBox";
 import Error from "./Error";
 import UseSearchParam from "../helper/UseSearchParam";
+import Pagination from "./Pagination";
 
 export default function AllQuestions({ history }) {
   const location = UseSearchParam(),
@@ -43,16 +43,6 @@ export default function AllQuestions({ history }) {
       );
   }, [currentPage, perPageLimit]);
 
-  const handleLimitChange = useCallback(
-    ({ target }) => {
-      const limitCount = +target.dataset.limit;
-      setPerPageLimit(limitCount || 10);
-      setCurrentPage(0);
-      history.push(`/questions?limit=${limitCount}`);
-    },
-    [history]
-  );
-
   return (
     <div
       className="row p-1"
@@ -87,69 +77,19 @@ export default function AllQuestions({ history }) {
                   />
                 ))}
               </div>
-              <div className="container">
-                <div className="pt-3 pb-1 px-md-5 d-flex flex-column-reverse flex-sm-row">
-                  <div className="mb-2 d-inline-block text-center gap-1 d-flex align-items-end justify-content-center limit-buttons">
-                    <div
-                      className={
-                        "btn btn-light border " +
-                        (+perPageLimit === 10 ? "active" : "")
-                      }
-                      data-limit="10"
-                      onClick={handleLimitChange}
-                    >
-                      10
-                    </div>
-                    <div
-                      className={
-                        "btn btn-light border " +
-                        (+perPageLimit === 20 ? "active" : "")
-                      }
-                      data-limit="20"
-                      onClick={handleLimitChange}
-                    >
-                      20
-                    </div>
-                    <div
-                      className={
-                        "btn btn-light border " +
-                        (+perPageLimit === 30 ? "active" : "")
-                      }
-                      data-limit="30"
-                      onClick={handleLimitChange}
-                    >
-                      30
-                    </div>
-                    items per page
-                  </div>
-                  <div className="flex-grow-1"></div>
-                  <ReactPaginate
-                    previousLabel={"<<"}
-                    nextLabel={">>"}
-                    breakLabel={"..."}
-                    marginPagesDisplayed={5}
-                    pageCount={Math.ceil(questionsLength / perPageLimit)}
-                    pageRangeDisplayed={2}
-                    onPageChange={(pageData) => {
-                      setCurrentPage(pageData.selected);
-                      history.push(`/questions?page=${+pageData.selected + 1}`);
-                    }}
-                    containerClassName={
-                      "pagination justify-content-center align-items-end mb-2"
-                    }
-                    pageClassName={"page-item"}
-                    pageLinkClassName={"page-link"}
-                    previousClassName={"page-item"}
-                    previousLinkClassName={"page-link"}
-                    nextClassName={"page-item"}
-                    nextLinkClassName={"page-link"}
-                    breakClassName={"page-item"}
-                    breakLinkClassName={"page-link"}
-                    activeClassName={"active"}
-                    forcePage={+currentPage}
-                  />
-                </div>
-              </div>
+              <Pagination
+                {...{
+                  limitValues: [10, 20, 30],
+                  limitsMessage: "questions per page",
+                  itemsLength: questionsLength,
+                  perPageLimit,
+                  setPerPageLimit,
+                  currentPage,
+                  setCurrentPage,
+                  history,
+                  route: "/questions"
+                }}
+              />
             </>
           ) : (
             <Error
---
+++ client/src/components/Pagination.jsx
@@ -0,0 +1,72 @@
+import React, { useCallback } from "react";
+import ReactPaginate from "react-paginate";
+
+export default function Pagination({
+  limitValues,
+  limitsMessage,
+  itemsLength,
+  perPageLimit,
+  setPerPageLimit,
+  currentPage,
+  setCurrentPage,
+  history,
+  route
+}) {
+  const handleLimitChange = useCallback(
+    ({ target }) => {
+      const limitCount = +target.dataset.limit;
+      setPerPageLimit(limitCount || 10);
+      setCurrentPage(0);
+      history.push(`${route}?limit=${limitCount}`);
+    },
+    [history, setCurrentPage, setPerPageLimit, route]
+  );
+
+  return (
+    <div className="container">
+      <div className="pt-3 pb-1 px-md-5 d-flex flex-column-reverse flex-sm-row">
+        <div className="mb-2 d-inline-block text-center gap-1 d-flex align-items-end justify-content-center limit-buttons">
+          {limitValues.map((l) => (
+            <div
+              key={"limit-" + l}
+              className={
+                "btn btn-light border " + (+perPageLimit === +l ? "active" : "")
+              }
+              data-limit={l}
+              onClick={handleLimitChange}
+            >
+              {l}
+            </div>
+          ))}
+          {limitsMessage}
+        </div>
+        <div className="flex-grow-1"></div>
+        <ReactPaginate
+          previousLabel={"<<"}
+          nextLabel={">>"}
+          breakLabel={"..."}
+          marginPagesDisplayed={5}
+          pageCount={Math.ceil(itemsLength / perPageLimit)}
+          pageRangeDisplayed={2}
+          onPageChange={(pageData) => {
+            setCurrentPage(pageData.selected);
+            history.push(`${route}?page=${+pageData.selected + 1}`);
+          }}
+          containerClassName={
+            "pagination justify-content-center align-items-end mb-2"
+          }
+          pageClassName={"page-item"}
+          pageLinkClassName={"page-link"}
+          previousClassName={"page-item"}
+          previousLinkClassName={"page-link"}
+          nextClassName={"page-item"}
+          nextLinkClassName={"page-link"}
+          breakClassName={"page-item"}
+          breakLinkClassName={"page-link"}
+          activeClassName={"active"}
+          forcePage={+currentPage}
+        />
+      </div>
+    </div>
+  );
+}
