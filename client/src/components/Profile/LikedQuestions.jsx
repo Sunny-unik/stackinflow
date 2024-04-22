e98cba24accb40fd1f83e9a9e22cf6a481d97d72
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-04-23 00:02:55 +0530

    enhance: ui/ux of likedQuestions

--- client/src/components/Profile/LikedQuestions.jsx
+++ client/src/components/Profile/LikedQuestions.jsx
@@ -1,47 +1,82 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import handleDate from "../../helper/dateHelper";
 import QuestionBox from "../QuestionBox";
+import Error from "../Error";
+import Spinner from "../loadings/Spinner";
+import { NavLink } from "react-router-dom/cjs/react-router-dom";
 
-export default function Likedquestions() {
-  const user = useSelector((state) => state.user);
-  const uid = useSelector((state) => state.user._id);
-  const [question, setquestion] = useState([]);
+export default function LikedQuestions() {
+  const user = useSelector((state) => state.user),
+    userId = user._id,
+    [questions, setQuestions] = useState({
+      data: null,
+      loading: true,
+      error: null
+    });
 
   useEffect(() => {
     axios
-      .get(`${process.env.REACT_APP_API_URL}/question/list`)
-      .then((res) => setquestion(res.data.data))
-      .catch((err) => console.log(err));
-  }, []);
-
-  const userliked = [];
-  question.forEach((all) => {
-    return all.qlikes.forEach((u) => {
-      if (u === uid) userliked.push(all);
-    });
-  });
+      .get(
+        `${process.env.REACT_APP_API_URL}/question/user-liked?userId=${userId}`
+      )
+      .then((res) =>
+        setQuestions({ data: res.data.data, loading: false, error: null })
+      )
+      .catch((error) => setQuestions({ error, loading: false, data: null }));
+  }, [userId]);
 
   return (
     <div className="border" style={{ minHeight: "64vh" }}>
       <h1 className="p-1 pb-2" style={{ borderBottom: "2px solid lightgrey" }}>
         &nbsp;All following questions are liked by {user.dname}.
       </h1>
-      {userliked.map((q) => (
-        <QuestionBox
-          key={q._id + "_selfLiked"}
-          questionId={q._id}
-          likesCount={q.qlikes.length}
-          questionTitle={q.question}
-          answersCount={q.answers ? q.answers.length : 0}
-          tags={q.tags}
-          dataAos={"fade-left"}
-          userObj={q.userId ? q.userId : (q.userId = { dname: "404" })}
-          date={q.date}
-        />
-      ))}
+      {questions.loading ? (
+        <Spinner />
+      ) : (
+        <>
+          {questions.data?.length ? (
+            <>
+              {questions.data.map((q) => (
+                <QuestionBox
+                  key={q._id + "_selfLiked"}
+                  questionId={q._id}
+                  likesCount={q.qlikes.length}
+                  questionTitle={q.question}
+                  answersCount={q.answers ? q.answers.length : 0}
+                  tags={q.tags}
+                  dataAos={"fade-left"}
+                  userObj={q.userId ? q.userId : (q.userId = { dname: "404" })}
+                  date={q.date}
+                />
+              ))}
+            </>
+          ) : (
+            <>
+              {Array.isArray(questions.data) ? (
+                <Error
+                  heading="Not found"
+                  statusCode="404"
+                  message={NotLikedMessage()}
+                />
+              ) : (
+                <Error />
+              )}
+            </>
+          )}
+        </>
+      )}
     </div>
+  );
+}
+
+function NotLikedMessage() {
+  return (
+    <>
+      <span className="fw-bold fs-5">
+        Ooh, You didn't like any question for now,&nbsp;
+        <NavLink to="/">start exploring questions</NavLink>.
+      </span>
+    </>
   );
 }
