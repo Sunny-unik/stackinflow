a183c91b9efc0b277ad8c9731ad88cd9baa3edf3
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-04-24 18:06:23 +0530

    enhance: askedQuestions state updates

--- client/src/components/Profile/AskedQuestions.jsx
+++ client/src/components/Profile/AskedQuestions.jsx
@@ -3,44 +3,67 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { NavLink } from "react-router-dom";
-import handleDate from "../../helper/dateHelper";
 import QuestionBox from "../QuestionBox";
+import Error from "../Error";
+import Spinner from "../loadings/Spinner";
 
-export default function Askedquestions() {
-  const [question, setquestion] = useState([]);
+export default function AskedQuestions() {
   const user = useSelector((state) => state.user);
+  const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
     axios
-      .get(`${process.env.REACT_APP_API_URL}/question/list`)
-      .then((res) => setquestion(res.data.data))
-      .catch((err) => console.log(err));
-  }, []);
-
-  const askedquestion = question.filter((un) => un.userdname === user._id);
+      .get(
+        `${process.env.REACT_APP_API_URL}/question/per-user?userId=${user._id}`
+      )
+      .then((res) =>
+        setQuestions({ data: res.data.data, loading: false, error: null })
+      )
+      .catch((error) => console.log({ error, data: null, loading: false }));
+  }, [user._id]);
 
   return (
     <div className="border" style={{ minHeight: "64vh" }}>
       <h1 className="p-1 pb-2" style={{ borderBottom: "2px solid lightgrey" }}>
         &nbsp;All following questions are posted by {user.dname}.
       </h1>
-      <div>
-        {askedquestion &&
-          askedquestion.map((q) => (
-            <QuestionBox
-              key={q._id + "_askedQuestion"}
-              questionId={q._id}
-              likesCount={q.qlikes.length}
-              questionTitle={q.question}
-              answersCount={q.answers ? q.answers.length : 0}
-              tags={q.tags}
-              dataAos={"fade-left"}
-              userObj={q.userId ? q.userId : (q.userId = { dname: "404" })}
-              date={q.date}
-            />
-          ))}
-      </div>
+      {questions.loading ? (
+        <Spinner />
+      ) : (
+        <>
+          {questions.data?.length ? (
+            <>
+              {questions.data.map((q) => (
+                <QuestionBox
+                  key={q._id + "_askedQuestion"}
+                  questionId={q._id}
+                  likesCount={q.qlikesCount}
+                  questionTitle={q.question}
+                  answersCount={q.answersCount}
+                  tags={q.tags}
+                  dataAos={"fade-left"}
+                  userObj={
+                    q.userId ? q.userId : (q.userId = { dname: user.dname })
+                  }
+                  date={q.date}
+                />
+              ))}
+            </>
+          ) : (
+            <>
+              {Array.isArray(questions.data) ? (
+                <Error
+                  heading="Not found"
+                  statusCode="404"
+                  message={"You have not posted any question"}
+                />
+              ) : (
+                <Error />
+              )}
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 }
